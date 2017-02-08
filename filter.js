7732904e97552c376737e0a1ab0a5c1f1514262a
jurjenborkent <jurjenborkent@gmail.com>
2017-02-08 09:51:48 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,41 @@
+function vraag1() {
+
+$("#functie_2").toggleClass("normal flipped")
+$("#functie_7").toggleClass("normal flipped")
+$("#functie_10").toggleClass("normal flipped")
+$("#functie_11").toggleClass("normal flipped")
+$("#functie_5").toggleClass("normal flipped")
+
+document.getElementById('knopvraag1').style.display='none';
+
+}
+
+function vraag2() {
+
+$('#functie_6').toggleClass("normal flipped")
+$('#functie_3').toggleClass("normal flipped")
+
+document.getElementById('knopvraag2').style.display='none';
+
+}
+
+function vraag3() {
+
+$('#functie_12').toggleClass("normal flipped")
+$('#functie_4').toggleClass("normal flipped")
+$('#functie_9').toggleClass("normal flipped")
+$('#functie_1').toggleClass("normal flipped")
+
+document.getElementById('knopvraag3').style.display='none';
+
+}
+
+function vraag4() {
+
+document.getElementById('knopvraag4').style.display='none';
+
+}
+
+
+
+// On div with id functie_1 toggle between flipped and niks
